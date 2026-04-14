7ffa21d0ed59b7232626a96e4408c35850910d77
cvanzetten <c.vanzetten@uniglobealliancetravel.nl>
2026-04-14 11:37:24 +0200

    Update journey.js

--- api/journey.js
+++ api/journey.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   try {
     const r = await fetch(
-      `https://v5.db.transport.rest/journeys?from=${from}&to=${to}&results=1`
+      `/api/journey?from=${from}&to=${to}&results=1`
     );
     const data = await r.json();
 
